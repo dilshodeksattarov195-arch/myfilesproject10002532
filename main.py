1caf8c76c070b2d808ed74e72f0b04c0d820917e
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 15:24:10 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class routerUalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = routerUalculateWorker()
+    print("Worker engine initialized.")
